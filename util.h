c67573c82797338fcd48002b88414436588de733
Joshua Permito <git.commits@dwigoric.dev>
2025-03-23 18:31:20 +0800

    feat: add generator util function

--- util.h
+++ util.h
@@ -1,8 +1,14 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <chrono>
+#include <random>
 #include <string>
 #include <unordered_map>
+
+inline std::default_random_engine generator(
+    static_cast<unsigned>(std::chrono::system_clock::now().time_since_epoch().count())
+);
 
 std::tuple<std::string, unsigned short> parse_line(const std::string &line);
 
